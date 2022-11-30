f9004bfc9cdab26779e278961db017e628c9cf35
lijin <www.lijin@foxmail.com>
2022-11-30 11:15:48 +0800

    测试编译

--- app/src/main/java/com/jk/basis/MainActivity.kt
+++ app/src/main/java/com/jk/basis/MainActivity.kt
@@ -11,7 +11,7 @@ import android.view.Menu
 import android.view.MenuItem
 import com.jk.basis.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : JKBaseActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
---
+++ basis/src/main/java/com/jk/basis/JKBaseActivity.kt
@@ -0,0 +1,11 @@
+package com.jk.basis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+open class JKBaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
